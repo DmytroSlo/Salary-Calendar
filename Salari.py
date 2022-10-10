af99ec71559e65932964bfc7a8035b3a7ee51112
DmytroSlo <112366206+DmytroSlo@users.noreply.github.com>
2022-10-10 17:26:44 +0200

    V0.8

--- Salari.py
+++ Salari.py
@@ -3,6 +3,9 @@ import os
 import sqlite3
 import dbcommand as dc
 from tkcalendar import DateEntry, Calendar
+import calendar
+from babel.dates import format_date, parse_date, get_day_names, get_month_names
+from babel.numbers import *
 from tkinter import ttk
 from tkinter import *
 from tkinter import Menu
@@ -105,6 +108,76 @@ def from_submit():
         msg = "Не всі поля є заповнені"
         mb.showerror("Валідація", msg)
 
+#Редактирование данных
+
+#Валидация
+def validation_upgrade():
+    return len(start_box_new.get()) != 0 and len(finish_box_new.get()) != 0 and len(work_time_box_new.get()) != 0
+
+
+def from_update():
+    edyt_win = Toplevel()
+    edyt_win.title("Изминение данных")
+    edyt_win.geometry("330x200")
+    edyt_win.resizable(False, False)
+
+    data_new = tk.Label(edyt_win, text = "Дата", font = ("Sylfaen", 12))
+    data_new.pack()
+    data_new.place(x = 10, y = 10)
+    data_box_new = DateEntry(edyt_win, foreground = 'White', normalforeground = 'black',
+                                        selectforeground = 'red', beckground = 'white',
+                                        date_pattern = 'dd.mm.YYYY', locale="ru", state = 'readonly')
+    data_box_new.pack()
+    data_box_new.place(x = 150, y = 10)
+    start_new = tk.Label(edyt_win, text = "Початок праці", font = ("Sylfaen", 12))
+    start_new.pack()
+    start_new.place(x = 10, y = 40)
+    start_box_new = tk.Entry(edyt_win, font = ("Sylfaen", 10))
+    start_box_new.pack()
+    start_box_new.place(x = 150, y = 40)
+    finish_new = tk.Label(edyt_win, text = "Кінець праці", font = ("Sylfaen", 12))
+    finish_new.pack()
+    finish_new.place(x = 10, y = 70)
+    finish_box_new = tk.Entry(edyt_win, font = ("Sylfaen", 10))
+    finish_box_new.pack()
+    finish_box_new.place(x = 150, y = 70)
+    work_time_new = tk.Label(edyt_win, text = "Години", font = ("Sylfaen", 12))
+    work_time_new.pack()
+    work_time_new.place(x = 10, y = 100)
+    work_time_box_new = tk.Entry(edyt_win, font = ("Sylfaen", 10))
+    work_time_box_new.pack()
+    work_time_box_new.place(x = 150, y = 100)
+
+    sub = tk.Button(edyt_win, text = "Записати", font = ("Sylfaen", 10), command = upd)
+    sub.pack()
+    sub.place(x = 10, y = 150)
+    
+def upd():    
+    if validation_upgrade():
+        Data_new = data_box_new.get()
+        Start_new = start_box_new.get()
+        Finish_new = finish_box_new.get()
+        Work_new = work_time_box_new.get()
+        Saler_new = work_time_box_new.get()
+        add_calendar_new = (Data_new, Start_new, Finish_new, Work_new, Saler_new)
+        with sqlite3.connect('db/database.db') as db:
+            cursor = db.cursor()
+            query = """ UPDATE calendar SET(Дата, Початок_праці, Кінець_праці, Години, Зароблено) 
+                                            WHERE (?, ?, ?, ?, (?*22.7)); """
+            cursor.execute(query, add_calendar_new)
+            db.commit()
+
+        edyt_win.destroy()
+        window.destroy()
+        os.popen("Salari.py")
+    else:
+        msg = "Не всі поля є заповнені"
+        mb.showerror("Валідація", msg)
+
+
+ed = tk.Button(frame_statistics, text = "Редагувати", font = ("Sylfaen", 12), command = from_update)
+ed.pack()
+ed.place(x = 10, y = 140)
 
 #Флажки
 r_var = BooleanVar()
